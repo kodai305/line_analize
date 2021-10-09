4ffcddf8844872ca2dcda08064f8b143a61b3db1
Kodai Takagi <takagi.kodai@murata.com>
2021-10-09 13:40:56 +0900

    analize sum of message

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,98 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-textarea
+      v-model="talktext"
+      counter
+      label="トーク履歴"
+      no-resize
+      outlined
+      placeholder="LINEのトーク履歴を貼り付け"
+      rows="5"
+    />
+    <v-btn
+      elevation="2"
+      @click="onClickedAnalize"
+    >
+      Analize
+    </v-btn>
+    <p>
+      総メッセージ数: {{ talks.length }}
+    </p>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+interface Talk {
+  date: Date,
+  sender: String,
+  contents: String
+}
+
+@Component
+export default class Home extends Vue {
+  talktext: String = ''
+  talks: Talk[] = []
+
+  onClickedAnalize () {
+    // リセット
+    this.talks = []
+    // console.log(this.talktext)
+    const regexpDate = /^[0-9]{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])/
+    const regexpTime = /^[0-9]{2}:[0-9]{2}/
+    const regexpReceiver = /\[LINE\] (.*)とのトーク履歴/
+    const messages = this.talktext.split(/\n/)
+    let sendDate!: Date
+    let date!: String
+    let receiver!: String
+    let sender!: String
+    let contents!: String
+    for (const message of messages) {
+      // 送信先の名前を取得
+      if (message.match(regexpReceiver)) {
+        const matched = message.match(regexpReceiver)
+        console.log(matched)
+        receiver = String(matched![1])
+        continue
+      }
+      // 日付の取得
+      if (message.match(regexpDate)) {
+        const matched = message.match(regexpDate)
+        date = String(matched![0])
+        // console.log(date)
+        continue
+      }
+      // 時間から始まるメッセージ
+      if (message.match(regexpTime)) {
+        if (sendDate) {
+          const talk: Talk = {
+            date: sendDate,
+            sender,
+            contents
+          }
+          this.talks.push(talk)
+        }
+        const talkline = message.split(/\t/)
+        if (talkline.length !== 3) {
+          continue
+        }
+        const time = talkline[0]
+        sender = talkline[1]
+        contents = talkline[2]
+        sendDate = new Date(date + '/' + time)
+      } else {
+        // 改行されたメッセージを追加
+        contents = contents + message
+      }
+    }
+    const talk: Talk = {
+      date: sendDate,
+      sender,
+      contents
+    }
+    this.talks.push(talk)
+    console.log(this.talks)
+  }
+}
+</script>
